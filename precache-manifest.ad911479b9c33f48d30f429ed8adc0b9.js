b75fc16d72916dddb897b378a470ad17801d1222
Ayi05 <fernandajavon@hotmail.com>
2020-02-13 03:26:28 -0500

    Updates

---
+++ precache-manifest.ad911479b9c33f48d30f429ed8adc0b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98d732a8e3410b6110bfa4f63e130e51",
+    "url": "/demo-restapi/index.html"
+  },
+  {
+    "revision": "86c7abb7e4e1b45d269e",
+    "url": "/demo-restapi/static/css/main.31a580ac.chunk.css"
+  },
+  {
+    "revision": "0d139c43ad1b5a98a689",
+    "url": "/demo-restapi/static/js/2.ff5870cd.chunk.js"
+  },
+  {
+    "revision": "e933f8dbfdf496cb72e495663a499a00",
+    "url": "/demo-restapi/static/js/2.ff5870cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86c7abb7e4e1b45d269e",
+    "url": "/demo-restapi/static/js/main.dfb87f0e.chunk.js"
+  },
+  {
+    "revision": "00616ac8bf989ebdbce1",
+    "url": "/demo-restapi/static/js/runtime-main.bbf3793e.js"
+  }
+]);
