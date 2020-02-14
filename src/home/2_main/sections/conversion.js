c624802681420b1e122ce31aa5d214cc68d62881
Ayi05 <fernandajavon@hotmail.com>
2020-02-14 01:28:38 -0500

    Deploy and currency call api

--- src/home/2_main/sections/conversion.js
+++ src/home/2_main/sections/conversion.js
@@ -4,30 +4,12 @@ import MenuItem from '@material-ui/core/MenuItem';
 import { Button } from '@material-ui/core';
 import { ValidatorForm, TextValidator, SelectValidator } from 'react-material-ui-form-validator';
 
-const currencies = [
-    {
-        value: '1.087',
-        label: 'USD',
-    },
-    {
-        value: '1',
-        label: 'EUR',
-    },
-    {
-        value: '1.442',
-        label: 'CAD',
-    },
-    {
-        value: '655.957',
-        label: 'CFA',
-    },
-];
 
 const useStyles = makeStyles(theme => ({
     root: {
         '& .MuiTextField-root': {
             margin: theme.spacing(1),
-            width: 200,
+            width: 160,
         },
     },
 }));
@@ -43,12 +25,14 @@ export default function Conversion() {
 
 
     const handleCurrencyIn = event => {
-        setCurrencyIn(event.target.value);
+        let i = event.target.value;
+        setCurrencyIn(currencies[i]);
         setAmountOut('');
     };
 
     const handleCurrencyOut = event => {
-        setCurrencyOut(event.target.value);
+        let o = event.target.value;
+        setCurrencyOut(currencies[o]);
         setAmountOut('');
     };
 
@@ -63,73 +47,86 @@ export default function Conversion() {
             amount = amount.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1 ');
             setAmountOut(amount);
         }
-        // else setAmountOut('Montant Invalide')
     };
+
+    const [currencies, setCurrencies] = React.useState([]);
+
+    React.useEffect(() => {
+        fetch('https://api.exchangeratesapi.io/latest')
+            .then(res => res.json())
+            .then(data => setCurrencies(data.rates))
+    });
+
+    const currenciesName = Object.keys(currencies);
 
     return (
         <div className="d-flex justify-content-center">
-            <ValidatorForm className={classes.root} autoComplete="on" onSubmit={handleSubmit}>
 
-                <div className="form-group row my-4">
-                    <SelectValidator
-                        select
-                        size="small"
-                        label="De"
-                        value={currencyIn}
-                        onChange={handleCurrencyIn}
-                        helperText=""
-                        variant="outlined">
+            <div className="border border-info rounded pb-3">
+                <ValidatorForm className={classes.root} autoComplete="on" onSubmit={handleSubmit}>
 
-                        {currencies.map(option => (
-                            <MenuItem key={option.value} value={option.value}>
-                                {option.label}
-                            </MenuItem>
-                        ))}
+                    <div className="form-inline my-3">
+                        <div className="input-group-append">
+                            <SelectValidator
+                                select
+                                size="small"
+                                label="De"
+                                onChange={handleCurrencyIn}
+                                helperText=""
+                                variant="outlined">
 
-                    </SelectValidator>
+                                {currenciesName.map(option => (
+                                    <MenuItem key={option} value={option}>
+                                        {option}
+                                    </MenuItem>
+                                ))}
 
-                    <TextValidator
-                        size="small"
-                        type="number"
-                        label="Montant"
-                        value={amountIn}
-                        onChange={handleAmountIn}
-                        variant="outlined"
-                        validators={['required', 'minNumber:0', 'maxNumber:9999999999999999']} errorMessages={['Champ Obligatoire!']}
-                    />
-                </div>
+                            </SelectValidator>
 
-                <div className="form-group row my-4">
-                    <SelectValidator
-                        select
-                        size="small"
-                        label="À"
-                        value={currencyOut}
-                        onChange={handleCurrencyOut}
-                        helperText=""
-                        variant="outlined"
-                    >
-                        {currencies.map(option => (
-                            <MenuItem key={option.value} value={option.value}>
-                                {option.label}
-                            </MenuItem>
-                        ))}
-                    </SelectValidator>
-                    <TextValidator
-                        size="small"
-                        value={amountOut}
-                        variant="outlined"
-                        helperText=""
-                        InputProps={{ readOnly: true, }}
-                    />
+                            <TextValidator
+                                size="small"
+                                type="number"
+                                label="Montant"
+                                value={amountIn}
+                                onChange={handleAmountIn}
+                                variant="outlined"
+                                validators={['required', 'minNumber:0', 'maxNumber:9999999999999999']} errorMessages={['Champ Obligatoire!']}
+                            />
+                        </div>
+                    </div>
 
+                    <div className="form-inline my-3">
+                        <div className="input-group-append">
+                            <SelectValidator
+                                select
+                                size="small"
+                                label="À"
+                                onChange={handleCurrencyOut}
+                                helperText=""
+                                variant="outlined"
+                            >
+                                {currenciesName.map(option => (
+                                    <MenuItem key={option} value={option}>
+                                        {option}
+                                    </MenuItem>
+                                ))}
+                            </SelectValidator>
 
-                </div>
-                <div className="text-center">
-                    <Button variant="contained" color="primary" type="submit"><i className="fa fa-angle-double-right"></i></Button>
-                </div>
-            </ValidatorForm>
+                            <TextValidator
+                                size="small"
+                                value={amountOut}
+                                variant="outlined"
+                                helperText=""
+                                InputProps={{ readOnly: true, }}
+                            />
+                        </div>
+                    </div>
 
-        </div>
+                    <div className="text-center">
+                        <Button variant="contained" color="primary" type="submit"><i className="fa fa-angle-double-right"></i></Button>
+                    </div>
+                </ValidatorForm>
+            </div>
+        </div >
     );
 }
--- src/home/2_main/sections/users.js
+++ src/home/2_main/sections/users.js
@@ -6,36 +6,58 @@ const Users = () => {
     const [users, setUsers] = useState([]);
 
     useEffect(() => {
-        fetch('http://jsonplaceholder.typicode.com/users')
+        fetch('https://jsonplaceholder.typicode.com/users/')
             .then(res => res.json())
             .then(data => setUsers(data))
     });
 
     return (
 
-        <div className="border">
-            <table className="table table-md table-hover">
-                <thead className="bg-light">
-                    <tr>
-                        <th>Name</th>
-                        <th>E-Mail</th>
-                        <th>City</th>
-                        <th>Company</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {users.map(user => (
-                        <tr className="">
-                            <td><small>{user.name}</small></td>
-                            <td><small>{user.email}</small></td>
-                            <td><small>{user.address.city}</small></td>
-                            <td><small>{user.company.name}</small></td>
+        <>
+            {/* Visible only on xs and sm screen  */}
+            <div className="border d-md-none">
+                <table className="table table-md table-hover">
+                    <thead className="bg-light">
+                        <tr>
+                            <th>Name</th>
+                            <th>Company</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
+                    </thead>
+                    <tbody>
+                        {users.map(user => (
+                            <tr className="">
+                                <td><small>{user.name}</small></td>
+                                <td><small>{user.company.name}</small></td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
 
+            {/* Visible only on md screen and above */}
+            <div className="border d-none d-md-block">
+                <table className="table table-md table-hover">
+                    <thead className="bg-light">
+                        <tr>
+                            <th>Name</th>
+                            <th>E-Mail</th>
+                            <th>City</th>
+                            <th>Company</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users.map(user => (
+                            <tr className="">
+                                <td><small>{user.name}</small></td>
+                                <td><small>{user.email}</small></td>
+                                <td><small>{user.address.city}</small></td>
+                                <td><small>{user.company.name}</small></td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </>
     )
 };
 export default Users;
